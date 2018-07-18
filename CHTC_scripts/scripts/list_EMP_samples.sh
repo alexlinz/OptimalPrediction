509fde840f452e87832f2dd4ef8cf10b9668a8a7
Alex Linz <amlinz16@gmail.com>
2018-07-18 11:40:21 -0500

    Removed old structure, added new run instructions file under Code

--- CHTC_scripts/scripts/list_EMP_samples.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#mkdir /mnt/gluster/amlinz/NTL-MO/UW-processed/
-for file in /mnt/gluster/amlinz/NTL-MO/EMP/*.fastq.gz; do
-	sample=$(basename $file .fastq.gz);
-	echo $sample;
-	done > emp_samples.txt
--- CHTC_scripts/scripts/list_processed_UW_samples.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in /mnt/gluster/amlinz/NTL-MO/UW-processed/*joined.fastq.gz; do
-	sample=$(basename $file .fastq.gz);
-	echo $sample;
-	done > processed_uwbc_samples.txt
